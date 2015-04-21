b6ada760ec8a7a9a0e8b4542d64b6680347354b3
JoePaxton <JoePaxton@users.noreply.github.com>
2015-04-20 21:29:55 -0400

    added

---
+++ build2.py
@@ -0,0 +1,45 @@
+import echonest.remix.audio as audio
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def main():
+    filename = audio.LocalAudioFile("Treehome95.wav")
+    duration = filename.duration
+    print "\n\nDuration in seconds: " , duration
+    segments = audio.AudioAnalysis("Treehome95.wav").segments  
+    collect = audio.AudioQuantumList()
+    collect_t = audio.AudioQuantumList()
+    
+    for seg in segments:
+        collect.append(seg.pitches)
+        collect_t.append(seg.timbre)
+    x=0
+    y=0    
+    count=0
+   
+    print 'Length: ',len(segments)
+    points = np.zeros((len(segments), 3),dtype=float)
+    for i in range(len(segments)):        
+        points[i] = ( (i, np.mean(collect[i]), np.mean(collect_t[i])) )    
+        fig = plt.figure()
+        ax = fig.gca(projection = '3d')
+        ax.set_xlabel('Segments')
+        ax.set_ylabel('Pitches')
+        ax.set_zlabel('Timbre')
+        ax.set_title('3D Timbre and Pitches')
+        ax.scatter(points[:, 0], points[:, 1],  points[:, 2], zdir = 'z', c = '.5')
+        count = count + 1
+        fn = str('Scatter_%05d' % count) + '.png'
+        print '\rIterating through segments...',i,fn,		
+        plt.savefig(fn,dpi=75) 
+    
+    print "Number of images: ", count+1
+    fps = (count+1) / duration
+    print "FPS: ", fps
+    print "ffmpeg parameter for framerate: ", 1/fps
+    
+    return
+
+if __name__ == '__main__':
+    main()
---
+++ fft.py
@@ -0,0 +1,82 @@
+import echonest.remix.audio as audio
+from pyechonest.track import track_from_file
+import wave
+import struct
+import numpy as np
+from math import sqrt
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pylab
+import matplotlib.pyplot as plt
+
+filename = "Treehome95.wav"
+ 
+if __name__ == '__main__':
+    fileW = audio.LocalAudioFile(filename)
+    trackTitle = open(filename)
+    trackName = track_from_file(trackTitle, 'wav')
+    sampleRate = fileW.sampleRate
+    duration = fileW.duration
+    processLen = int(duration)-1
+    fileW = wave.open(filename, 'r')
+    dataSize = fileW.getnframes()
+    read = fileW.readframes(dataSize)
+    fileW.close()
+                
+    unpack = '%dh' % (dataSize)
+    read = struct.unpack(unpack, read)
+    
+    fps = 24
+    fwidth = 1.0/fps
+    sampleSize = fwidth * float(sampleRate)
+    transforms = int(round(processLen * fps))
+ 
+    def getBandWidth():
+		return (2.0 / sampleSize) * (sampleRate / 2.0)
+ 
+    def freqToIndex(f):
+		if f < getBandWidth()/2:
+			return 0
+		if f > (sampleRate / 2) - (getBandWidth() / 2):
+			return sampleSize -1
+		
+		fraction = float(f) / float(sampleRate)
+		index = round(sampleSize * fraction)
+		return index
+		
+    fftavg = []
+    
+    def average_fft_bands(fft_array):
+        numBands = 12
+        del fftavg[:]
+        for band in range(0, numBands):
+            avg = 0.0
+            if band == 0:
+                lowFreq = int(0)
+            else:
+                lowFreq = int(int(sampleRate / 2) / float(2 ** (numBands - band)))		  
+            hiFreq = int((sampleRate / 2) / float(2 ** ((numBands-1) - band)))
+            lowBound = int(freqToIndex(lowFreq))
+            upperBound = int(freqToIndex(hiFreq))
+            for j in range(lowBound, upperBound):
+                avg += fft_array[j]			
+            avg /= (upperBound - lowBound + 1)
+            fftavg.append(avg)
+
+    x = range(0, 12)
+    for offset in range(0, transforms):
+		start = int(offset * sampleSize)
+		end = int((offset * sampleSize) + sampleSize -1)
+		print "\rProcessing sample %i of %i (%d seconds)" % (offset + 1, transforms, end/float(sampleRate)),
+		sampleRange = read[start:end]
+		fft = abs(np.fft.fft(sampleRange))
+		fft *= ((2**.5)/sampleSize)
+		plt.ylim(0, 1000)
+		average_fft_bands(fft)
+		y = fftavg
+		width = 0.35
+		plt.title(trackName)
+		p1 = plt.bar(x, y, width, color='r')
+		filename = str('frame_%05d' % offset) + '.png'
+		plt.savefig(filename, dpi=100)
+		plt.close()
